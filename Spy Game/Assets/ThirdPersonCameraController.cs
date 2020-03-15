b6e77734f113c7e7397dbdfbbb23e0c245ea6b12
Philipp-sama123 <Philipp.Stampfer@protonmail.ch>
2020-03-15 03:11:03 +0100

    set up basic locomotion, camera, controller wip

---
+++ Spy Game/Assets/ThirdPersonCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 1;
+    [SerializeField] Transform Target, Player;
+    private float mouseX, mouseY;
+
+/// <summary>
+/// Start is called on the frame when a script is enabled just before
+/// any of the Update methods is called the first time.
+/// </summary>
+void Start()
+{
+    Cursor.visible = false; 
+    Cursor.lockState= CursorLockMode.Locked; 
+}
+    void LateUpdate()
+    {
+        CameraControl();
+    }
+
+    private void CameraControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
